2baa81587d52d15240f3db2df54d245adbaedb94
Kevin RodBe <kevin.rodriguez992@gmail.com>
2023-03-17 19:14:31 -0500

    feat: init

---
+++ main.js
@@ -0,0 +1,18 @@
+import './styles/common.scss';
+import './styles/home.scss';
+
+VANTA.BIRDS({
+  el: '.canvas-bg',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundColor: 0xffffff,
+  separation: 90.0,
+  alignment: 50.0,
+  cohesion: 60.0,
+  quantity: 2.3,
+});
